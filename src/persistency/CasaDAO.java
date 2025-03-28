3261c7e81bf868da32b4327e99cfee9bcd76dbb0
Amilcar-Steban <amilcarshm@gmail.com>
2025-03-27 20:41:31 -0500

    Casa DAO + Cliente

--- src/persistency/CasaDAO.java
+++ src/persistency/CasaDAO.java
@@ -1,5 +1,100 @@
 package persistency;
 
+import entity.Casa;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CasaDAO  extends DAO{
+
+    public CasaDAO() {
+
+    }
+    //agregar, eliminar, listar, buscar, modificar
+    public void agregarCasa(Casa casa) throws Exception {
+        try {
+            String sql = "INSERT INTO casa (id_casa, calle, numero, codigo_postal, ciudad, pais, fechaDesde, fechaHasta, tiempoMinimo, tiempoMaximo, precioHabitacion, tipoVivienda) VALUES ("
+                    + casa.getIdCasa() + "', '"
+                    + casa.getCalle() + "', "
+                    + casa.getNumero() + ", '"
+                    + casa.getCodigoPostal() + "', '"
+                    + casa.getCiudad() + "', '"
+                    + casa.getPais() + "', '"
+                    + casa.getFechaDesde() + "', '"
+                    + casa.getFechaHasta() + "', "
+                    + casa.getTiempoMinimo() + ", "
+                    + casa.getTiempoMaximo() + ", "
+                    + casa.getPrecioHabitacion() + ", '"
+                    + casa.getTipoVivienda() + "')";
+            crud(sql);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void listarCasas() throws Exception {
+        try {
+            String sql = "SELECT * FROM casas";
+            consultarDataBase(sql);
+            List<Casa> casas = new ArrayList<>();
+            while (resultSet.next()) {
+                Casa casa = new Casa();
+                casa.setIdCasa(resultSet.getInt("id_casa"));
+                casa.setCalle(resultSet.getString("calle"));
+                casa.setNumero(resultSet.getInt("numero"));
+                casa.setCodigoPostal(resultSet.getString("codigo_postal"));
+                casa.setCiudad(resultSet.getString("ciudad"));
+                casa.setPais(resultSet.getString("pais"));
+                casa.setFechaDesde(resultSet.getString("fechaDesde"));
+                casa.setFechaHasta(resultSet.getString("fechaHasta"));
+                casa.setTiempoMinimo(resultSet.getInt("tiempoMinimo"));
+                casa.setTiempoMaximo(resultSet.getInt("tiempoMaximo"));
+                casa.setPrecioHabitacion(resultSet.getDouble("precioHabitacion"));
+                casa.setTipoVivienda(resultSet.getString("tipoVivienda"));
+                casas.add(casa);
+            }
+            closeDataBase();
+            for (Casa cas : casas) {
+                System.out.println(cas.toString());
+                System.out.println("--------------------");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void eliminarCasa(int id) throws Exception {
+        try {
+            String sql = "DELETE FROM casa WHERE id_casa = " + id;
+            crud(sql);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void buscarCasa(int id) throws Exception {
+        try {
+            String sql = "SELECT * FROM casa WHERE id_casa = " + id;
+            consultarDataBase(sql);
+            Casa casa = new Casa();
+            if(resultSet.next()) {
+                casa.setIdCasa(resultSet.getInt("id_casa"));
+                casa.setCalle(resultSet.getString("calle"));
+                casa.setNumero(resultSet.getInt("numero"));
+                casa.setCodigoPostal(resultSet.getString("codigo_postal"));
+                casa.setCiudad(resultSet.getString("ciudad"));
+                casa.setPais(resultSet.getString("pais"));
+                casa.setFechaDesde(resultSet.getString("fechaDesde"));
+                casa.setFechaHasta(resultSet.getString("fechaHasta"));
+                casa.setTiempoMinimo(resultSet.getInt("tiempoMinimo"));
+                casa.setTiempoMaximo(resultSet.getInt("tiempoMaximo"));
+                casa.setPrecioHabitacion(resultSet.getDouble("precioHabitacion"));
+                casa.setTipoVivienda(resultSet.getString("tipoVivienda"));
+                System.out.println(casa.toString());
+            }
+            closeDataBase();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
--- src/persistency/ComentarioDAO.java
+++ src/persistency/ComentarioDAO.java
@@ -1,5 +1,72 @@
 package persistency;
 
+import entity.Comentario;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComentarioDAO  extends DAO{
+
+    public ComentarioDAO() {
+    }
+    //agregar, eliminar, listar, buscar, modificar
+    public void agregarComentario(Comentario comentario) throws Exception {
+        try {
+            String sql = "INSERT INTO comentario (id_comentario, id_casa, comentario) VALUES ("
+                    + comentario.getIdComentario() + ", "
+                    + comentario.getIdCasa() + ", '"
+                    + comentario.getComentario() + "')";
+            crud(sql);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void listarComentarios() throws Exception {
+        try {
+            String sql = "SELECT * FROM comentarios";
+            consultarDataBase(sql);
+            List<Comentario> comentarios = new ArrayList<>();
+            while (resultSet.next()) {
+                Comentario comentario = new Comentario();
+                comentario.setIdComentario(resultSet.getInt("id_comentario"));
+                comentario.setIdCasa(resultSet.getInt("id_casa"));
+                comentario.setComentario(resultSet.getString("comentario"));
+                comentarios.add(comentario);
+            }
+            closeDataBase();
+            for (Comentario com : comentarios) {
+                System.out.println(com.toString());
+                System.out.println("--------------------");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void eliminarComentario(int id) throws Exception {
+        try {
+            String sql = "DELETE FROM comentario WHERE id_comentario = " + id;
+            crud(sql);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void buscarComentario(int id) throws Exception {
+        try {
+            String sql = "SELECT * FROM comentario WHERE id_comentario = " + id;
+            consultarDataBase(sql);
+            Comentario comentario = new Comentario();
+            while (resultSet.next()) {
+                comentario.setIdComentario(resultSet.getInt("id_comentario"));
+                comentario.setIdCasa(resultSet.getInt("id_casa"));
+                comentario.setComentario(resultSet.getString("comentario"));
+            }
+            closeDataBase();
+            System.out.println(comentario.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
